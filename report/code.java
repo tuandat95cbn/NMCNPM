c221c8248972435c1777a378e0d04230984fa667
Tuấn Đạt Nguyễn <tuandat95cbn@gmail.com>
2016-01-10 01:48:48 +0700

    UpDate báo cáo

---
+++ report/code.java
@@ -0,0 +1,28 @@
+public boolean thayDoiTTMonAn(int trangThai) {
+	
+		boolean xd=false;
+		
+		if (dsMonAn==null) return false;
+		
+		if((trangThai<-1) && (trangThai >2)) return false;
+		
+		Iterator<MonAn> it = dsMonAn.iterator();
+		
+		while(it.hasNext()){
+			
+			if (checkTT(it)==false) {
+				
+				it.settrangThai(trangThai);
+				xd=true
+			}
+			it.next();
+		}
+		if (xd==false) System.out.printf("Khong thay doi gi");
+		
+		
+		return true;
+		
+}
+
+
+
